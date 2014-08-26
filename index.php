c94c4a3c0323b1aa20b5dc8d9838590a48e0d54f
razvan <razvan.veina@gmail.com>
2014-08-26 14:40:44 +0300

    Improve menu

--- index.php
+++ index.php
@@ -1,40 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <HTML>
-<HEAD>
-<TITLE>Preturi supermarket</TITLE>
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1"/>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-</HEAD>
 
+		<?php require 'menu.php'?>
 
-<BODY>
-	<TABLE WIDTH="1010" BORDER="0" align="center" CELLPADDING="0"
-		CELLSPACING="0" ID="Table_01">
-		<TR>
-			<TD WIDTH="4" ROWSPAN="5" BGCOLOR="#124989"><IMG
-				SRC="images/spacer.gif" WIDTH="4" HEIGHT="300" ALT="" /></TD>
-			<TD WIDTH="1000" HEIGHT="100" class="companyname2">
-				<table BORDER="0">
-					<tr>
-						<td width="1000"><IMG src="images/belowMenu.png" /></td>
-					</tr>
-				</table>
-			</TD>
-			<TD WIDTH="4" ROWSPAN="5" BGCOLOR="#124989"><IMG
-				SRC="images/spacer.gif" WIDTH="4" HEIGHT="5" ALT="" /></TD>
-		</TR>
-		<TR>
-		<?php require 'menu.php'?>		 
-		</TR>
-		<TR>
-			<TD WIDTH="1000" HEIGHT="6" colspan="1"><IMG
-				src="images/belowMenu.png" /></TD>
-		</TR>
-		<TR>
-			<TD HEIGHT="1307" valign="top" bgcolor="#FFFFFF">
-				<table width="687" border="0" align="center" cellpadding="0"
-					cellspacing="0">
-					<tr>
 						<td class="content">
 							<TABLE border="0" cellpadding="2">
 <?php
--- menu.php
+++ menu.php
@@ -1,37 +1,55 @@
 <?php 
 $search = $_POST['search'];
 ?>
-<FORM action="search.php" method="POST">
-<TD id="menu" colspan="1">
-            <a href="index.php">
-              Home
-              &nbsp;
-            </a>
-            <a href="preturiSupermarket.php">
-              Preturi
-              &nbsp;
-            </a>
-            <a href="produse.php">
-              Produse
-              &nbsp;
-            </a>
-            <a href="contribute.php">
-              Contribuie
-              &nbsp;
-            </a>
-            <!-- div class="fb-like" data-href="http://www.razvanveina.ro" data-width="100" data-layout="button_count" data-show-faces="true" data-send="true"></div-->
-      <input type="text" class="tftextinput" name="search" size="15" maxlength="120" value="<?php echo $search ?>"><input type="submit" value="Cauta" class="tfbutton">
-            
-          </TD>
-      </FORM>
+<HEAD>
+<TITLE>Preturi supermarket</TITLE>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1"/>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/menustyles.css" rel="stylesheet" type="text/css">
+<script type='text/javascript' src='js/jquery.min.js'></script>
+<script type='text/javascript' src='js/menu_jquery.js'></script>
+
+</HEAD>
+
+
+<BODY>
+	<TABLE WIDTH="1010" BORDER="0" align="center" CELLPADDING="0"
+		CELLSPACING="0" ID="Table_01">
+		<TR>
+			<TD WIDTH="4" ROWSPAN="5" BGCOLOR="#124989"><IMG
+				SRC="images/spacer.gif" WIDTH="4" HEIGHT="300" ALT="" /></TD>
+			<TD WIDTH="1000" HEIGHT="100" class="companyname2">
+				<table BORDER="0">
+					<tr><FORM action="search.php" method="POST">					
+						<td width="1000" align="right">     <input type="text" class="tftextinput" name="search" size="15" maxlength="120"><input type="submit" value="Cauta" class="tfbutton">
+						</td>
+						</FORM>
+					</tr>
+				</table>
+			</TD>
+			<TD WIDTH="4" ROWSPAN="5" BGCOLOR="#124989"><IMG
+				SRC="images/spacer.gif" WIDTH="4" HEIGHT="5" ALT="" /></TD>
+		</TR>
+		<TR><TD>
+
+
+  <div id='cssmenu' class="rowMenu">
+  <ul>
+     <li class=''><a href='index.php'><span>Acasa</span></a></li>
+     <li class=''><a href='preturiSupermarket.php'><span>Preturi</span></a></li>
+     <li class='last'><a href='produse.php'><span>Produse</span></a></li>
+     <!-- li class='last'><a href='contribuie.php'><span>Contribuie</span></a></li-->`
+  </ul>
+  </div> 
           
-<!-- A href="addStoreForm.php">Add store</A>&nbsp;
-<A href="viewStores.php">View stores</A>&nbsp;
-<A href="addBrandForm.php">Add brand</A>&nbsp;
-<A href="viewBrands.php">View brands</A>&nbsp;
-<A href="addCategoryForm.php">Add category</A>&nbsp;
-<A href="viewCategories.php">View categories</A>&nbsp;
-<A href="addProductForm.php">Add product</A>&nbsp;
-<A href="addPriceForm.php">Add price</A>&nbsp;
-<BR/>
-<A href="search.php">Search</A&nbsp;-->
+		</TD>		 
+		</TR>
+		<TR>
+			<TD WIDTH="1000" HEIGHT="6" colspan="1"><IMG
+				src="images/belowMenu.png" /></TD>
+		</TR>
+		<TR>
+			<TD HEIGHT="1307" valign="top" bgcolor="#FFFFFF">
+				<table width="687" border="0" align="center" cellpadding="0"
+					cellspacing="0">
+					<tr>
